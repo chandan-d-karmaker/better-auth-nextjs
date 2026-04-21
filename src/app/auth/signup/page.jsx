ee197ab35b9085bd222328db2cd15f09d3ff24a0
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-04-21 21:25:27 +0600

    setup auth route and installed heroUI and gravity icons

---
+++ src/app/auth/signup/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react';
+import {Check} from "@gravity-ui/icons";
+import {Button, Description, FieldError, Form, Input, Label, TextField} from "@heroui/react";
+
+const SignupPage = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default SignupPage;
